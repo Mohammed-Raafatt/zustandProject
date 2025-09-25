a5483d9a502cfea7531a9044b2464828e696e675
Mohammed-Raafatt <dimoraafat@gmail.com>
2025-09-25 16:33:22 +0200

    edit my dit

--- src/Components/Login/Login.tsx
+++ src/Components/Login/Login.tsx
@@ -4,9 +4,7 @@ export default function Login() {
 
   return <>
      <h1>Login</h1>
-     <div>
-      <h1>welcome to new login page pro</h1>
-     </div>
+
      
   </>
 }
---
+++ src/Components/Welcome/Welcome.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Welcome() {
+
+  return <>
+     <h1>Welcome</h1>
+     <p>hi man</p>
+     
+     
+  </>
+}
