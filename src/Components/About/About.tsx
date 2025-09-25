ff701d953edaf49e7893674981df026945bc6668
Mohammed-Raafatt <dimoraafat@gmail.com>
2025-09-25 17:13:41 +0200

    add ingo to about component

--- src/Components/About/About.tsx
+++ src/Components/About/About.tsx
@@ -3,5 +3,8 @@ export default function About() {
 
   return <>
      <h1>helooo</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+       Illum odit repellat aspernatur. Ipsa vitae tempore cumque
+        deleniti, natus esse iure!</p>
   </>
 }
